18f156b108864e0bbdc99db8b092cef3c02f0819
Antonio Groza <me@epsilon.pw>
2017-04-16 17:58:36 +0200

    Add wait till drop

--- mobile.py
+++ mobile.py
@@ -1,67 +1,109 @@
-import requests
-from pprint import pprint
+import requests, time, telepot, datetime, pause
+from pytz import timezone
+from dateutil import tz
+
+to_buy = []
+
+keyword = "truth"
+fsize = "large"
+color = "red"
+uid = 174865824
 
 user_agent = "Mozilla/5.0 (iPad; CPU OS 10_2 like Mac OS X) AppleWebKit/602.3.12 (KHTML, like Gecko) Mobile/14C92"
+
+# Set up telegram bot
+bot = telepot.Bot("")
+
+start = time.time()
 session = requests.Session()
 
-def buy_product(json):
-    text = session.get(
-        "http://supremenewyork.com/shop/%d.json" %(json['id']),
+# Wait till 10 minutes before a drop
+drop_time = timezone('US/Eastern').localize(
+    datetime.datetime.strptime("20/04/2017 10:57", "%d/%m/%Y %H:%M")
+)
+
+print(drop_time.astimezone(tz.tzlocal()))
+
+pause.until(drop_time.astimezone(tz.tzlocal()))
+
+while len(to_buy) == 0:
+
+    # Find things to buy
+    resp = session.get("http://supremenewyork.com/mobile_stock.json",headers={
+        "User-agent" : user_agent
+        }).json()['products_and_categories']
+
+    # Flatten arrays
+    flattened_items = [h for i in resp for h in resp[i]]
+
+    # Remove duplicates
+    known_names = []
+    items = []
+
+    for i in flattened_items:
+        if not i['name'] in known_names:
+            items.append(i)
+            known_names.append(i['name'])
+
+    to_buy = [i for i in items if keyword in i['name'].lower()]
+
+    if len(to_buy) == 0:
+        print("Nothing yet")
+        time.sleep(1)
+
+for item in to_buy:
+    # Get product info
+    product_page = session.get(
+        "http://supremenewyork.com/shop/%d.json" %(item['id']),
         headers={
             "User-agent" : user_agent
         }
     ).json()
 
-    style = text['styles'][0]
-    size = text['styles'][0]['sizes'][0]
+    # Find size and style
+    style = {}
+    for s in product_page['styles']:
+        if s['name'].lower() in color.lower():
+            style = s
 
-    print(session.post(
-        "http://www.supremenewyork.com/shop/%d/add.json" %(json['id']),
-        data={"size":size['id'], "style":style['id'], "qty":"1"},
-        headers={
-            "User-agent" : user_agent
-        }
-    ).text)
+    size = 0
+    for s in style['sizes']:
+        if s['name'].lower() == fsize.lower() and s['stock_level'] > 0:
+            size = s['id']
 
-    # TODO Style selction algo
+    # Add to cart
+    add_to_cart = session.post(
+        "http://www.supremenewyork.com/shop/%d/add.json" %(item['id']),
+        data={"style" : style['id'], "size":size, "qty":"1"},
+        headers={ "User-agent" : user_agent }
+    )
 
-    params = {
-        "from_mobile" : "1",
-        "same_as_billing_address" : "1",
-        "order[billing_name]" : "A b",
-        "order[email]" : "a@b.com",
-        "order[tel]" : "0418485923",
-        "order[billing_address]" : "Jggj khbhkb 78",
-        "order[billing_city]" : "Hbkhb",
-        "order[billing_state]" : "",
-        "order[billing_zip]" : "47869",
-        "order[billing_country]" : "FI",
-        "credit_card[type]" : "visa",
-        "credit_card[cnb]" : "4242 4242 4242 4242",
-        "credit_card[month]" : "06",
-        "credit_card[year]" : "2017",
-        "credit_card[vval]" : "456",
-        "order[terms]" : "1",
-        "order[terms]" : "1",
-        "g-recaptcha-response" : "no",
-        "is_from_ios_native" : "1",
-        }
+# Checkout
+params = {
+    "from_mobile" : "1",
+    "same_as_billing_address" : "1",
+    "order[billing_name]" : "A b",
+    "order[email]" : "a@b.com",
+    "order[tel]" : "0418485923",
+    "order[billing_address]" : "Jggj khbhkb 78",
+    "order[billing_city]" : "Hbkhb",
+    "order[billing_state]" : "",
+    "order[billing_zip]" : "47869",
+    "order[billing_country]" : "FI",
+    "credit_card[type]" : "visa",
+    "credit_card[cnb]" : "4242 4242 4242 4242",
+    "credit_card[month]" : "06",
+    "credit_card[year]" : "2017",
+    "credit_card[vval]" : "456",
+    "order[terms]" : "1",
+    "g-recaptcha-response" : "no",
+    "is_from_ios_native" : "1",
+}
 
-    x = session.post("https://www.supremenewyork.com/checkout.json", data=params, headers={
-        "User-agent" : user_agent
-    }).text
+response = session.post("https://www.supremenewyork.com/checkout.json", data=params, headers={
+    "User-agent" : user_agent
+}).text
+took = time.time() - start
 
-    print (x)
 
-wanted_product = ["Supreme®/Duralex® Glasses (Set of 4)",
-                  "Supreme Truth Tour Jacket"]
-
-items = session.get("http://supremenewyork.com/mobile_stock.json",
-    headers={
-        "User-agent" : user_agent
-    }).json()
-
-for i in items["products_and_categories"]:
-    for h in items["products_and_categories"][i]:
-        if h["name"] in wanted_product:
-            buy_product(h)
+bot.sendMessage(uid, "%s in %ss" %(response, took))
